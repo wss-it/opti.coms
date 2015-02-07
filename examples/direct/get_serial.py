4ee6a03b0cfc84e24b3637fbfbd7316e46818268
MaZderMind <github@mazdermind.de>
2015-02-07 22:12:35 +0100

    restructure examples

--- examples/direct/get_serial.py
+++ examples/direct/get_serial.py
@@ -2,15 +2,17 @@
 
 import sys, os
 
-sys.path.insert(0, os.path.abspath(os.path.join(__file__, "../../module/")))
+sys.path.insert(0, os.path.abspath(os.path.join(__file__, "../../../module/")))
 import pydfcom
 
-print("connect")
+print("connect()")
 pydfcom.connect(
 	"10.6.100.107",  # host
 	8000,            # port
 	3000)            # read-timeout
 
-print("get_serial")
-print(pydfcom.get_serial());
+print("get_serial() = %d" %
+	pydfcom.get_serial());
+
+print("disconnect()")
 pydfcom.disconnect()
